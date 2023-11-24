1173298980711322953d84495fa156b7814e9fa4
shun198 <shunhiroseluvmri@gmail.com>
2023-11-24 21:08:37 +0900

    ✨ create color button app

---
+++ color-button-app/src/App.test.jsx
@@ -0,0 +1,22 @@
+// import { logRoles } from '@testing-library/dom';
+import { test, describe } from 'vitest';
+import { render, screen ,fireEvent } from '@testing-library/react';
+import App from './App';
+
+describe('ボタンを押すテスト', () => {
+  test('ボタンを押す前と後の色とラベルが期待通り', () => {
+    // デバッグ用
+    // const { container } = render(<App />);
+    // logRoles
+    // Appコンポーネントをレンダリング
+    render(<App />);
+    const buttonElement = screen.getByRole('button')
+    expect(buttonElement).toHaveTextContent("blue")
+    expect(buttonElement).toHaveClass('red');
+    // ボタンを押下
+    fireEvent.click(buttonElement)
+    expect(buttonElement).toHaveTextContent("red")
+    expect(buttonElement).toHaveClass('blue');
+  });
+});
+
