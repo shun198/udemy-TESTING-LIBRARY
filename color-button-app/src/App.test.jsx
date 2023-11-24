94dd146b8f25c9c495c9a46137e0f4b70d0fabc5
shun198 <shunhiroseluvmri@gmail.com>
2023-11-24 21:26:49 +0900

    👍 add eslint

--- color-button-app/src/App.test.jsx
+++ color-button-app/src/App.test.jsx
@@ -3,12 +3,36 @@ import { test, describe } from 'vitest';
 import { render, screen ,fireEvent } from '@testing-library/react';
 import App from './App';
 
-describe('ボタンを押すテスト', () => {
+describe('color button app', () => {
   test('ボタンを押す前と後の色とラベルが期待通り', () => {
     // デバッグ用
     // const { container } = render(<App />);
     // logRoles
     // Appコンポーネントをレンダリング
+    render(<App />);
+    const buttonElement = screen.getByRole('button')
+    expect(buttonElement).toHaveTextContent("blue")
+    expect(buttonElement).toHaveClass('red');
+    // ボタンを押下
+    fireEvent.click(buttonElement)
+    expect(buttonElement).toHaveTextContent("red")
+    expect(buttonElement).toHaveClass('blue');
+  });
+  test('ボタンを押す前と後の色とラベルが期待通り', () => {
+    // デバッグ用
+    // const { container } = render(<App />);
+    // logRoles
+    // Appコンポーネントをレンダリング
+    render(<App />);
+    const buttonElement = screen.getByRole('button')
+    expect(buttonElement).toHaveTextContent("blue")
+    expect(buttonElement).toHaveClass('red');
+    // ボタンを押下
+    fireEvent.click(buttonElement)
+    expect(buttonElement).toHaveTextContent("red")
+    expect(buttonElement).toHaveClass('blue');
+  });
+  test('チェックボックス用のテスト', () => {
     render(<App />);
     const buttonElement = screen.getByRole('button')
     expect(buttonElement).toHaveTextContent("blue")
