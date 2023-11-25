cd5a4c7e39c3db24918be96b4cabc9c332eedfa5
shun198 <shunhiroseluvmri@gmail.com>
2023-11-25 09:34:58 +0900

    👍 add checkbox

--- color-button-app/src/App.jsx
+++ color-button-app/src/App.jsx
@@ -3,16 +3,22 @@ import { useState } from 'react';
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  const className = disabled ? 'grey' : buttonColor;
 
   return (
     <div>
       <button
-        className={buttonColor}
+        className={className}
         onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
+      <br/>
+      <input type="checkbox" id="disable-button-checkbox" onChange={(e) => setDisabled(e.target.checked)}></input>
+      <label htmlfor="disable-button-checkbox">Disable Button</label>
     </div>
   );
 }
--- color-button-app/src/App.test.jsx
+++ color-button-app/src/App.test.jsx
@@ -4,21 +4,7 @@ import { render, screen ,fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('color button app', () => {
-  test('ボタンを押す前と後の色とラベルが期待通り', () => {
-    // デバッグ用
-    // const { container } = render(<App />);
-    // logRoles
-    // Appコンポーネントをレンダリング
-    render(<App />);
-    const buttonElement = screen.getByRole('button')
-    expect(buttonElement).toHaveTextContent("blue")
-    expect(buttonElement).toHaveClass('red');
-    // ボタンを押下
-    fireEvent.click(buttonElement)
-    expect(buttonElement).toHaveTextContent("red")
-    expect(buttonElement).toHaveClass('blue');
-  });
-  test('ボタンを押す前と後の色とラベルが期待通り', () => {
+  test('ボタンを押す前と後の色とラベルが変わる', () => {
     // デバッグ用
     // const { container } = render(<App />);
     // logRoles
@@ -34,13 +20,37 @@ describe('color button app', () => {
   });
   test('チェックボックス用のテスト', () => {
     render(<App />);
+    const checkBoxElement = screen.getByRole('checkbox')
+    expect(checkBoxElement).toBeEnabled();
+    expect(checkBoxElement).not.toBeChecked();
+  });
+  test('チェックボックスがチェックされていたらボタンが有効化/無効化されるテスト', () => {
+    render(<App />);
     const buttonElement = screen.getByRole('button')
+    const checkBoxElement = screen.getByRole('checkbox')
+    expect(buttonElement).toBeEnabled();
+    expect(checkBoxElement).not.toBeChecked();
+    // チェックボックスを押下にボタンを無効化
+    fireEvent.click(checkBoxElement)
+    expect(buttonElement).toBeDisabled();
+    expect(checkBoxElement).toBeChecked();
+    // チェックボックスを押下にボタンを有効化
+    fireEvent.click(checkBoxElement)
+    expect(buttonElement).toBeEnabled();
+    expect(checkBoxElement).not.toBeChecked();
+  });
+  test('チェックボックスがチェックされていたらボタンの色が変わらないテスト', () => {
+    render(<App />);
+    const buttonElement = screen.getByRole('button')
+    const checkBoxElement = screen.getByRole('checkbox')
     expect(buttonElement).toHaveTextContent("blue")
-    expect(buttonElement).toHaveClass('red');
-    // ボタンを押下
+    expect(checkBoxElement).not.toBeChecked();
+    // チェックボックスを押下にボタンを無効化
+    fireEvent.click(checkBoxElement)
     fireEvent.click(buttonElement)
-    expect(buttonElement).toHaveTextContent("red")
-    expect(buttonElement).toHaveClass('blue');
+    expect(checkBoxElement).toBeChecked();
+    expect(buttonElement).toBeDisabled();
+    expect(buttonElement).toHaveTextContent("blue")
   });
 });
 
